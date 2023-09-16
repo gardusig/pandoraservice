63bf3564a436f46f8e9b03de75ab46bf3625a2fd
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-09-16 20:54:39 -0300

    Updated internals

--- internal/constant.go
+++ internal/constant.go
@@ -1,5 +1,12 @@
 package internal
 
+import "os"
+
+const (
+	pandoraPortEnvKey  = "PANDORA_PORT_ENV"
+	pandoraDefaultPort = "50051"
+)
+
 const (
 	MinThreshold = -4000000000000000000
 	MaxThreshold = +4000000000000000000
@@ -12,3 +19,12 @@ var (
 )
 
 var EncryptedMessage = "important encrypted message"
+
+var PandoraServicePort string
+
+func init() {
+	PandoraServicePort = os.Getenv(pandoraPortEnvKey)
+	if PandoraServicePort == "" {
+		PandoraServicePort = pandoraDefaultPort
+	}
+}
