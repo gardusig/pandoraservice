0e511dd04061db2363096fcb8545765fd8e869d8
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2023-09-16 20:54:25 -0300

    Added database struct to hold random number(s)

--- database/randomStorage.go
+++ database/randomStorage.go
@@ -6,19 +6,27 @@ import (
 	"github.com/gardusig/grpc_service/internal"
 )
 
-var randomNumber int64
-
-func ShuffleSpecialNumber() {
-	randomNumber = rand.Int63n(internal.MaxThreshold-internal.MinThreshold+1) + internal.MinThreshold
+type SpecialNumberDb struct {
+	randomNumber int64
 }
 
-func ValidateGuess(guess int64) string {
-	if guess < randomNumber {
+func NewSpecialNumberDb() *SpecialNumberDb {
+	return &SpecialNumberDb{
+		randomNumber: getRandomNumber(),
+	}
+}
+
+func (db *SpecialNumberDb) ValidateGuess(guess int64) string {
+	if guess < db.randomNumber {
 		return internal.Less
 	}
-	if guess > randomNumber {
+	if guess > db.randomNumber {
 		return internal.Greater
 	}
-	ShuffleSpecialNumber()
+	db.randomNumber = getRandomNumber()
 	return internal.Equal
+}
+
+func getRandomNumber() int64 {
+	return rand.Int63n(internal.MaxThreshold-internal.MinThreshold+1) + internal.MinThreshold
 }
